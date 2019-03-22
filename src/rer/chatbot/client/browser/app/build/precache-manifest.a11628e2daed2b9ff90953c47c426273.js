a03f154cafe1a0c84a89fb06ce302e283d1a5a98
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-03-22 11:55:55 +0100

    ad build

---
+++ src/rer/chatbot/client/browser/app/build/precache-manifest.a11628e2daed2b9ff90953c47c426273.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c8574910df40704bc06",
+    "url": "++plone++rer.chatbot.client/static/css/rer-chatbot.css"
+  },
+  {
+    "revision": "5c8574910df40704bc06",
+    "url": "++plone++rer.chatbot.client/static/js/rer-chatbot.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "++plone++rer.chatbot.client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0988e3a5cbff30984eef43cbd9080d9c",
+    "url": "++plone++rer.chatbot.client/index.html"
+  }
+];
---
+++ src/rer/chatbot/client/browser/app/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "++plone++rer.chatbot.client/precache-manifest.a11628e2daed2b9ff90953c47c426273.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("++plone++rer.chatbot.client/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
